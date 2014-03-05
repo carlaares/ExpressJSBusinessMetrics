831fc7d06825d48404a6ea0cd6880f9b933fc1c2
carla <carla@honou.com.ar>
2014-03-05 14:57:19 -0300

    list collections inside a database

--- routes/collection.js
+++ routes/collection.js
@@ -1,10 +1,17 @@
 /* GET collections listing. */
 exports.list = function(db) {
   return function(req, res) {
-    db.collectionNames(function(err, collections){
-      console.log(collections);
+    var collection = db.collection('databases');
+    var ObjectID = require('mongodb').ObjectID;
+    collection.find({ _id: new ObjectID(req.params.database_id) }, function (err, post) {
+
+console.log(post.name);
+      var connection = require('mongoskin').db("mongodb://localhost/"+post.name, { native_parser:true });
+      connection.collectionNames(function(err, collections){
+        console.log(collections);
+        return res.render('collections', { title: 'Collections on '+post.name, collections: collections });
+      });
     });
-    res.json('a');
   }
 };
 
