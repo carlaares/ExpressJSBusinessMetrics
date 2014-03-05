8bf9c7603859dfba43e066862231100ade184cd9
carla <carla@honou.com.ar>
2014-03-05 13:16:31 -0300

    database list working

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,16 @@
 /* GET home page. */
 exports.index = function(db){
-  var collection = db.collection('databases');
-  var dbs = collection.find().toArray();
-  res.render('index', { title: 'Express', databases: dbs });
+  return function(req, res) {
+    var collection = db.collection('databases');
+    var dbs;
+    collection.find().toArray(function(err, docs) {
+      dbs = docs;
+    });
+    var dbs_count = 0;
+    collection.count(function(err, count) {
+      dbs_count = count;
+    });
+
+    res.render('index', { title: 'Express', databases_count: dbs_count, databases: dbs });
+  }
 };
